065a1115ce2c5bd1e84f10f521bb7d0d9d7d225c
chamounhahim <emile.chamoun@hotmail.com>
2020-04-27 14:32:01 -0400

    Lowercase email before saving to the DB

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,5 +7,6 @@ class User < ApplicationRecord
                          uniqueness: {case_sensitive: false},
                          length: { maximum: 105 },
                          format: { with: VALID_EMAIL_REGEX } 
+    before_save { self.email = email.downcase}
     has_many :articles
 end
