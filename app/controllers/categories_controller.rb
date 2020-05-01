92b8c95e157618f15ce46fc132e2b2160701f73a
chamounhahim <emile.chamoun@hotmail.com>
2020-05-01 13:55:25 -0400

    add categories model and test

---
+++ app/controllers/categories_controller.rb
@@ -0,0 +1,37 @@
+class CategoriesController < ApplicationController
+    before_action :set_category, only: [:show, :edit, :update, :destroy]
+
+    def index
+        @categories = Category.paginate(page: params[:page], per_page: 5)
+
+    end
+
+    def new
+        @category = Category.new
+    end
+
+    def create
+        @category = Category.new(category_params)
+        if @category.save
+            flash[:notice] = "La catégorie a été crée"
+            redirect_to categories_path
+        else
+            render 'new' 
+        end
+
+    end
+
+    def show
+        
+    end
+
+    private 
+
+    def set_category
+        @category = Category.find(params[:id]) 
+    end
+
+    def category_params
+        params.require(:category).permit(:name) 
+    end
+end
