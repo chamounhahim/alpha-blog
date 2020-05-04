c80a5b1194ae277a5d8007c1b16bd208b4192c1b
chamounhahim <emile.chamoun@hotmail.com>
2020-05-04 11:29:11 -0400

    Add edit category name feature for admins

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,6 +1,6 @@
 class CategoriesController < ApplicationController
     before_action :set_category, only: [:show, :edit, :update, :destroy]
-    before_action :require_admin, only: [:new, :create]
+    before_action :require_admin, only: [:new, :create, :edit, :update]
 
     def index
         @categories = Category.paginate(page: params[:page], per_page: 5)
@@ -20,6 +20,19 @@ class CategoriesController < ApplicationController
             render 'new' 
         end
 
+    end
+
+    def edit
+        
+    end
+
+    def update
+        if @category.update(category_params)
+            flash[:success] = "Le nom de la catégorie a été modifié"
+            redirect_to category_path(@category)
+        else
+            render 'edit'
+        end
     end
 
     def show
